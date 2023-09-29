68076929c243767d8b8d4e0d70c5e7644ae0e5a2
Sundeep L <laxmsun@hotmail.com>
2023-09-29 08:12:23 +0000

    added logging statement in main()

--- week1/index_products.py
+++ week1/index_products.py
@@ -146,6 +146,7 @@ def main(source_dir: str, index_name: str, workers: int):
         futures = [executor.submit(index_file, file, index_name) for file in files]
         for future in concurrent.futures.as_completed(futures):
             docs_indexed += future.result()
+            logger.info(f'{docs_indexed} documents indexed')
 
     finish = perf_counter()
     logger.info(f'Done. Total docs: {docs_indexed} in {(finish - start)/60} minutes')
