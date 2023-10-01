de2b1d8bcca5806b80349c2a9ff013b0918fd7af
Sundeep L <laxmsun@hotmail.com>
2023-10-01 19:45:52 +0000

    fixed typo in query_string key

--- week1/search.py
+++ week1/search.py
@@ -120,7 +120,7 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
                                             "query": user_query,
                                             "fields": ["name^100", "shortDescription^50", "longDescription^10", "department"],
                                             "phrase_slop": 3
-                        },
+                        }
                     }
                 }
             ],  
