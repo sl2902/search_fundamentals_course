09c427f8a5717937843f115eb45e602572b9d9d2
Sundeep L <laxmsun@hotmail.com>
2023-10-01 18:57:14 +0000

    updated field mappings bbuy_products updated opensearch to add Opensearch instance updated search to add query string, filters and aggs

--- week1/search.py
+++ week1/search.py
@@ -94,8 +94,10 @@ def query():
     print("query obj: {}".format(query_obj))
 
     #### Step 4.b.ii
-    response = None   # TODO: Replace me with an appropriate call to OpenSearch
-    # Postprocess results here if you so desire
+    try:
+        response = opensearch.search(body=query_obj, index="bbuy_products")
+    except:
+        error = 999
 
     #print(response)
     if error is None:
@@ -111,11 +113,56 @@ def create_query(user_query, filters, sort="_score", sortDir="desc"):
     query_obj = {
         'size': 10,
         "query": {
-            "match_all": {} # Replace me with a query that both searches and filters
+                "query_string": {
+                            "query": user_query,
+                            "fields": ["name^100", "shortDescription^50", "longDescription^10", "department"],
+                            "phrase_slop": 3
+        },
+        "filter": filters
         },
         "aggs": {
-            #### Step 4.b.i: create the appropriate query and aggregations here
+            "regularPrice": {
+                "range": {
+                    "field": "regularPrice",
+                    "ranges": [
+                        {"key": "$", "to": 100},
+                        {"key": "$$", "from": 100, "to": 200},
+                        {"key": "$$$", "from": 200, "to": 300},
+                        {"key": "$$$$", "from": 300, "to": 400},
+                        {"key": "$$$$$", "from": 400, "to": 500},
+                        {"key": "$$$$$$", "from": 500},
+                    ]
+                }
+            },
+            "department": {
+                "terms": {
+                    "field": "department.keyword",
+                    "min_doc_count": 1
+                }
+            },
+            "missing_images": {
+                "missing": {
+                    "field": "image"
+                    }
+            }
 
+        },
+        "highlight": {
+            "fields": {
+                "name": {},
+                "shortDescription": {},
+                "longDescription": {}
+            }
+    },
+    "sort" : [
+        {
+            "regularPrice": {
+              "order" : sortDir
+            },
+            "name.keyword": {
+                "order": sortDir
+            }
         }
-    }
+    ]
+  }
     return query_obj
